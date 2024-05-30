ec2172dfb3cd9432a8d42cad3c7d72f9f2e3f97d
Sandip  Pradhan <158505928+sandip9851@users.noreply.github.com>
2024-05-30 17:35:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,20 +2,21 @@ const name = document.querySelector("#item-name-input");
 const price = document.querySelector("#item-price-input");
 const table =  document.querySelector("#cart-item");
 const btn = document.querySelector("#btn");
+const grandTotal = document.querySelector("[data-ns-test=grandTotal]"); // grand total element
 let total = 0;
 
 btn.addEventListener("click",()=>{
     let itemName = name.value;
-    let itemPrice = Number(price.value); // convert the price to a number
+    let itemPrice = Number(price.value);
     total += itemPrice;
-    let tbody = table.querySelector("tbody") || table.appendChild(document.createElement('tbody')); // get the tbody or create one if it doesn't exist
+    let tbody = table.querySelector("tbody") || table.appendChild(document.createElement('tbody'));
     tbody.innerHTML += `
         <tr>
             <td data-ns-test="item-name">${itemName}</td>
             <td data-ns-test="item-price">${itemPrice}</td>
-            <td data-ns-test="grandTotal">${total}</td>
         </tr>
     `;
-    name.value = ''; // clear the input fields
+    grandTotal.innerText = total; // update the grand total
+    name.value = '';
     price.value = '';
 })
