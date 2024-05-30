77845b48909bbabf458e5d3b2a4d52875a7d357f
Sandip  Pradhan <158505928+sandip9851@users.noreply.github.com>
2024-05-30 17:12:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your code here
+const name = document.querySelector("#item-name");
+const price = document.querySelector("#item-price");
+const table =  document.querySelector("#cart-item");
+const btn = document.querySelector("#btn");
+let total = 0;
+
+btn.addEventListener("click",()=>{
+    let itemName = name.value;
+    let itemPrice = Number(price.value); // convert the price to a number
+    total += itemPrice;
+    let tbody =  table.appendChild(document.createElement('tbody')); // get the tbody or create one if it doesn't exist
+    tbody.innerHTML += `
+        <tr>
+            <td data-ns-test="item-name">${itemName}</td>
+            <td data-ns-test="item-price">${itemPrice}</td>
+            <td data-ns-test="grandTotal">${total}</td>
+        </tr>
+    `;
+    name.value = ''; // clear the input fields
+    price.value = '';
+})
